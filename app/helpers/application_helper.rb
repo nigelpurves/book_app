5021219413da02a76bc96874a880f67e7e5859cf
Nigel Purves <nigelpurves@Nigel-Purvess-MacBook-Pro.local>
2012-06-08 16:30:20 +0100

    title change for all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Music App"
+    base_title = "Qusic"
     if page_title.empty?
       base_title
     else    
