20c82a329a35dca14a3f41e4fe2d4cf9b3e78d4b
Nigel Purves <nigelpurves@Nigel-Purvess-MacBook-Pro.local>
2012-06-13 08:14:01 +0100

    before messing with model & microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,7 +4,6 @@ class Micropost < ActiveRecord::Base
   attr_accessible :artist, :track
   belongs_to :user
 
-  
   validates :artist,  presence: true, length: { maximum: 140 }
   validates :track,   presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
