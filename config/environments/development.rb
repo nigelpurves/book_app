4588ce5f2d1f68189933380a449f8fdbdb64f0f6
Nigel Purves <nigelpurves@Nigel-Purvess-MacBook-Pro.local>
2012-07-06 18:21:26 +0100

    fully set up mail to be sent

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,22 @@ MusicApp::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.default_url_options = {
+    :host => 'qusic.herokuapp.com'
+  }
+
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :domain => 'qusic.herokuapp.com',
+    :user_name => 'qusic',
+    :password => 'nigeno5',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 # CHANGE THESE DETAILS FOR PRODUCTION
 
-ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "25",
-  :authentication => :plain,
-  :user_name      => "app424627@heroku.com",
-  :password       => "XXXXXXXX",
-  :domain         => "gocardless.com"
-}
+# ActionMailer::Base.smtp_settings = {
+#  :address        => "smtp.sendgrid.net",
+#  :port           => "25",
+#  :authentication => :plain,
+#  :user_name      => "app424627@heroku.com",
+#  :password       => "XXXXXXXX",
+#  :domain         => "gocardless.com"
+#}
 
 
