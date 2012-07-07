c13c94fb8fbc7fbcfc279e564d5733a6516a9d40
Nigel Purves <nigelpurves@(null).(none)>
2012-07-07 13:33:58 +0100

    sendgrid page added for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ MusicApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/sendgrid',to: 'static_pages#sendgrid'
 
   get "pages/home"
   get "pages/contact"
