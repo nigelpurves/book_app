335f096f1d783884293cfea588c227a05fcf99bd
Nigel Purves <nigelpurves@(null).(none)>
2012-07-07 14:53:12 +0100

    removed sendgrid authentication page

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ MusicApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  match '/sendgrid',to: 'static_pages#sendgrid'
 
   get "pages/home"
   get "pages/contact"
