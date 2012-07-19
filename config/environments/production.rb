53ed354bc567b571682b89eed9dab4155533f9df
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2012-07-19 10:56:51 +0100

    disabling ssl. can be moved to nginx for simple cases anyway.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,10 @@ MusicApp::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -50,9 +50,9 @@ MusicApp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  
+
   config.action_mailer.default_url_options = {
-   :host => 'qusic.herokuapp.com'
+   :host => 'qusic.co.uk'
    }
 
   config.action_mailer.smtp_settings = {
