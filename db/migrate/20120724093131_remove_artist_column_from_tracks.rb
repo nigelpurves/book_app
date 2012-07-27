edf0626bf468c380fe15fe4ca7439a0fd2d10095
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2012-07-27 16:20:09 +0100

    retrospectively changing migration for teh wyn.

--- db/migrate/20120724093131_remove_artist_column_from_tracks.rb
+++ db/migrate/20120724093131_remove_artist_column_from_tracks.rb
@@ -1,9 +1,19 @@
 class RemoveArtistColumnFromTracks < ActiveRecord::Migration
   def up
+    rename_column :tracks, :artist, :old_artist_name
+
+    Track.reset_column_information
+    Track.each do |track|
+      artist = Artist.find_or_create_by_name(track.old_artist_name)
+      track.artist = artist
+      track.save!
+    end
+
     remove_column(:tracks, :artist)
   end
 
   def down
     add_column(:tracks, :artist, :string)
   end
+
 end
