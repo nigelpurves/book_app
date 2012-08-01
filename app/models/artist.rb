52ded401f14c91d717bdc520825dbcf03c2ed13a
Nigel Purves <nigelpurves@Nigel-Purvess-MacBook-Pro.local>
2012-08-01 17:39:51 +0100

    started artist catalogue download plus made some CSS changes to the users show page

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,26 +2,30 @@ class Artist < ActiveRecord::Base
   require 'spotify'
   
   attr_accessible :name
-#  has_many :interests
   has_many :artist_interests
   has_many :track_interests, through: :tracks
   has_many :tracks
   
   validates :name,  presence: true, length: { maximum: 140 }
   
-  # need to do the below with a broader search, == is too narrow at the moment
+  def spotify_download_catalogue
+    spotify_artist_catalogue.each do |entry|
+      track_record = self.tracks.where('lower("tracks"."name") = ?', entry.name.downcase).first
+      if track_record.nil?
+        track_record = Track.new(name: entry.name, artist: self)
+        track_record.lookup_links
+        track_record.save
+      end
+    end
+  end
   
   def spotify_artist_catalogue
-    spotify_artist_search.select { |track| track.artists[0].name == "#{name}" }
-  end
-  
-  def spotify_artist_search
     result = []
     (1..100).each do |a| 
-      result << Spotify.search_track("#{name}", page: a)
+      result << Spotify.search_track(self.name, page: a)
     end
     r = result.compact.flatten
-    r.select { |track| track.album.availability['territories'].split(" ").include? "GB" }
+    c = r.select { |track| track.album.availability['territories'].split(" ").include? "GB" }
+    spotify_artist_catalogue = c.select { |track| track.artists[0].name.downcase == self.name.downcase }
   end
-  
 end
